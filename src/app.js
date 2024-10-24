858eb48d331aa1c3a5fbf1670b8deb473db56752
Humaima <riazhumaima@gmail.com>
2024-10-24 14:24:37 +0500

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
 import express from "express";
 const app = express();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: '16kb'}))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+//import routes
+import userRouter from './routes/user.routes.js'
+
+//routes description
+app.use("/api/v1/users", userRouter)
+
+//http://localhost:8000/api/v1/users/register
+
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-
+import {app} from './app.js';
 dotenv.config({
     path: './env'
 })
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
 
-app.use(express.json({limit: '16kb'}))
-app.use(express.urlencoded({extended: true}))
-app.use(express.static("public"))
-app.use(cookieParser())
 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`server is running ${process.env.PORT}`)
+        console.log(`server is running at port ${process.env.PORT}`)
     })
 })
 .catch((err) => {
-    console.log("mongodb is not connected", err);
+    console.log("MONGODB connection failed !!!", err);
     
 })
 
